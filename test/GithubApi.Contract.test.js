ae10c9e8e975286db034ee90e4eb095c04f5c2bf
veronicatofino <veronicatofino@gmail.com>
2021-01-22 10:54:17 -0500

    Adding Schema validation test

---
+++ test/GithubApi.Contract.test.js
@@ -0,0 +1,25 @@
+const agent = require('superagent');
+const chai = require('chai');
+const { listPublicEventsSchema } = require('./schema/ListPublicEvents.schema');
+
+const { expect } = chai;
+chai.use(require('chai-json-schema'));
+
+const urlBase = 'https://api.github.com';
+
+describe('Given event Github API resources', () => {
+  describe('When wanna verify the List public events', () => {
+    let response;
+
+    before(async () => {
+      response = await agent
+        .get(`${urlBase}/events`)
+        .set('User-Agent', 'agent')
+        .auth('token', process.env.ACCESS_TOKEN);
+    });
+
+    it('Then the body should have a schema', () => {
+      expect(response).to.be.jsonSchema(listPublicEventsSchema);
+    });
+  });
+});
