89ab743fd9219c2c2a3741e289d958c9696c89f0
veronicatofino <veronicatofino@gmail.com>
2021-01-22 07:52:06 -0500

    Update GithubApi.Users.test.js

--- test/GithubApi.Users.test.js
+++ test/GithubApi.Users.test.js
@@ -6,7 +6,7 @@ const responseTime = require('superagent-response-time');
 const urlBase = 'https://api.github.com';
 
 describe('Given a Github Api URL to do users test', () => {
-  describe('When want to bring all the users and check the response time of the request', () => {
+  describe('When want check the response time of the request and count how many users it brings', () => {
     let response;
     let rTime;
     const callback = (req, time) => {
@@ -25,55 +25,45 @@ describe('Given a Github Api URL to do users test', () => {
       expect(rTime).to.satisfy((num) => num < 5000);
     });
 
-    describe('When want to count how many users brings the request by default', () => {
-      let responseCount;
+    it('And the number of user should be 30', () => {
+      expect(response.status).to.equal(statusCode.OK);
+      expect(response.body).to.have.length(30);
+    });
+
+    describe('When want to bring only 10 users', () => {
+      let responseCountT;
+      const queryArgsT = {
+        per_page: 10
+      };
 
       before(async () => {
-        responseCount = await agent.get(`${urlBase}/users`)
+        responseCountT = await agent.get(`${urlBase}/users`)
+          .query(queryArgsT)
           .auth('token', process.env.ACCESS_TOKEN)
           .set('User-Agent', 'agent');
       });
 
-      it('Then the number of user should be 30', () => {
-        expect(response.status).to.equal(statusCode.OK);
-        expect(responseCount.body).to.have.length(30);
+      it('Then the number of users should be 10', () => {
+        expect(responseCountT.status).to.equal(statusCode.OK);
+        expect(responseCountT.body).to.have.length(10);
       });
 
-      describe('When want to bring only 10 users', () => {
-        let responseCountT;
-        const queryArgsT = {
-          per_page: 10
+      describe('When want to bring only 50 users', () => {
+        let responseCountF;
+        const queryArgsF = {
+          per_page: 50
         };
 
         before(async () => {
-          responseCountT = await agent.get(`${urlBase}/users`)
-            .query(queryArgsT)
+          responseCountF = await agent.get(`${urlBase}/users`)
+            .query(queryArgsF)
             .auth('token', process.env.ACCESS_TOKEN)
             .set('User-Agent', 'agent');
         });
 
-        it('Then the number of users should be 10', () => {
-          expect(responseCountT.status).to.equal(statusCode.OK);
-          expect(responseCountT.body).to.have.length(10);
-        });
-
-        describe('When want to bring only 50 users', () => {
-          let responseCountF;
-          const queryArgsF = {
-            per_page: 50
-          };
-
-          before(async () => {
-            responseCountF = await agent.get(`${urlBase}/users`)
-              .query(queryArgsF)
-              .auth('token', process.env.ACCESS_TOKEN)
-              .set('User-Agent', 'agent');
-          });
-
-          it('Then the number of users should be 50', () => {
-            expect(responseCountF.status).to.equal(statusCode.OK);
-            expect(responseCountF.body).to.have.length(50);
-          });
+        it('Then the number of users should be 50', () => {
+          expect(responseCountF.status).to.equal(statusCode.OK);
+          expect(responseCountF.body).to.have.length(50);
         });
       });
     });
